3e6f04b4e86884eeb24f9231b737b2688e36611b
zhaojunlike <1716771371@qq.com>
2017-05-13 15:38:46 +0800

    'new'

---
+++ app/conf/config.js
@@ -0,0 +1,27 @@
+/**
+ * Created by zhaojunlike on 2017/5/13.
+ */
+exports.API_ROUTES = {
+    trademark: {
+        hostname: 'japi.juhe.cn',
+        port: 80,
+        path: '/trademark/search?',
+        method: 'GET',
+        key: "6685f44a8611ea8fae3b71317fc64d25",
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
+        }
+    },
+    patent: {
+        hostname: 'v.juhe.cn',
+        port: 80,
+        path: '/patent/search.php?',
+        method: 'GET',
+        key: "e9135f40a861b8d5f52cab763f0a30d1",
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
+        }
+    },
+};
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,33 +4,7 @@ const http = require("http");
 const queryString = require("querystring");
 const spider = require("../spider/control");
 /* GET home page. */
-
-
-const API_ROUTES = {
-    trademark: {
-        hostname: 'japi.juhe.cn',
-        port: 80,
-        path: '/trademark/search?',
-        method: 'GET',
-        key: "6685f44a8611ea8fae3b71317fc64d25",
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
-        }
-    },
-    patent: {
-        hostname: 'v.juhe.cn',
-        port: 80,
-        path: '/patent/search.php?',
-        method: 'GET',
-        key: "e9135f40a861b8d5f52cab763f0a30d1",
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
-        }
-    },
-};
-
+const API_ROUTES = require('../conf/config');
 
 //http://www.juhe.cn, 13482264559  a12345
 
